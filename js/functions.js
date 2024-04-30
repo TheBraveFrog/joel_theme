8561f60036ff0bbf94706f84cc2c153f8ab50ccf
TheBraveFrog <113203035+TheBraveFrog@users.noreply.github.com>
2024-04-30 13:56:48 -0600

    footer menu

---
+++ js/functions.js
@@ -0,0 +1,5 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('menu-toggle-btn').addEventListener('click', function() {
+        this.classList.toggle('open');
+    });
+});
